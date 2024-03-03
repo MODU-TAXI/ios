fe9baf6934564b42eaf8104870531604c8d2cd3b
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-03-03 17:46:55 +0900

    MTIOS-3 setting eslint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: [
+    '.eslintrc.js',
+    'babel.config.js',
+    'jest.config.js',
+    'metro.config.js',
+    'tailwind.config.js',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
