7d0c9a93b7268ea38f1a98e06e1e9d27d6b0a12a
Seungbo Shim <fodo9898@gmail.com>
2024-03-29 20:15:05 +0900

    MI-59 adding components

---
+++ src/components/RoomDigest/RoomDigestBox.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Text, View, Pressable } from 'react-native';
+import { styled } from 'nativewind';
+
+import ClockFillSvg from '@assets/images/RoomDigest/ClockFill.svg';
+import LocationFillSvg from '@assets/images/RoomDigest/LocationFill.svg';
+import Person2FillSvg from '@assets/images/RoomDigest/Person2Fill.svg';
+import ArrowRightSvg from '@assets/images/RoomDigest/ArrowRight.svg';
+import RoomTagComponent from './RoomTag';
+
+const StyledView = styled(View);
+
+interface RoomDigestBoxProps {
+  //tags: string[];
+  lastChatTime: number;
+  ETD: string;
+  start: string;
+  destination: string;
+  currCount: number;
+  maxCount: number;
+  expense: number;
+}
+
+/** 카풀팟 방 미리보기 컴포넌트 */
+const RoomDigestBoxComponent = ({
+  lastChatTime,
+  ETD,
+  start,
+  destination,
+  currCount,
+  maxCount,
+  expense,
+}: RoomDigestBoxProps) => {
+  return (
+    <StyledView className="flex-1 border border-gray200 bg-white shadow-sm rounded-xl p-4 mb-4 w-full h-auto">
+      <View className="flex flex-row h-auto mb-4 justify-between items-center">
+        <View className="flex flex-row">
+          {/** TODO: tags에서 RoomTagComponent 인자 받아오기 */}
+          <RoomTagComponent
+            label="마감임박"
+            textColor="text-warning"
+            bgColor="bg-[#FCE6E6]"
+          />
+          <RoomTagComponent
+            label="학생인증"
+            textColor="text-main"
+            bgColor="bg-sub100"
+          />
+          <RoomTagComponent
+            label="여자만"
+            textColor="text-gray700"
+            bgColor="bg-box"
+          />
+        </View>
+        <Text className="text-xs text-gray500">{lastChatTime}분전 채팅</Text>
+      </View>
+      <View className="h-auto mb-4 text-gray800">
+        <View className="flex flex-row items-center">
+          <ClockFillSvg />
+          <Text className="font-semibold mb-1">{ETD}출발</Text>
+        </View>
+        <View className="flex flex-row items-center">
+          <LocationFillSvg />
+          <Text className="font-semibold mr-1">{start}</Text>
+          <ArrowRightSvg />
+          <Text className="font-semibold ml-1">{destination}</Text>
+        </View>
+      </View>
+      <View className="flex flex-row justify-between">
+        <View className="flex flex-row items-center ml-1">
+          <Person2FillSvg />
+          <Text className="text-xs text-gray500 ml-1">
+            {currCount}/{maxCount}
+          </Text>
+        </View>
+        <Text className="text-xs text-gray500">인당 {expense}원</Text>
+      </View>
+    </StyledView>
+  );
+};
+
+export default RoomDigestBoxComponent;
---
+++ src/components/RoomDigest/SearchLocationButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import MagnifyingGlassSvg from '@assets/images/RoomDigest/MagnifyingGlass.svg';
+import XmarkSvg from '@assets/images/RoomDigest/XmarkCircleFill.svg';
+
+interface SearchLocationButtonProps {
+  label: string;
+}
+
+/** 카풀팟 태그 필터 버튼 */
+const SearchLocationButtonComponent = ({
+  label,
+}: SearchLocationButtonProps) => {
+  return (
+    <View className="flex bg-box rounded-full mr-2 text-gray700">
+      <View className="flex flex-row px-2 py-1.5 items-center">
+        <MagnifyingGlassSvg width={20} height={20} />
+        <Text className="mx-1 text-gray700 text-sm">{label}</Text>
+        <XmarkSvg />
+      </View>
+    </View>
+  );
+};
+
+export default SearchLocationButtonComponent;
