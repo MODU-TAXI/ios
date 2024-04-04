de60ee3f882b3d6df69ec0730f6346b01961b612
Seungbo Shim <fodo9898@gmail.com>
2024-04-04 15:45:07 +0900

    MI-54 자모음 분리 해결

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { Pressable, Text, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  NativeSyntheticEvent,
+  Pressable,
+  Text,
+  TextInput,
+  TextInputChangeEventData,
+  View,
+} from 'react-native';
 import MagnifyingGlassMainSvg from '@assets/images/Search/MagnifyingGlassMain.svg';
 import { useNavigation } from '@react-navigation/native';
 
 interface SearchBoxProps {
   value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
 /** 검색 바 */
-const SearchBoxComponent = ({ value }: SearchBoxProps) => {
+const SearchBoxComponent: React.FC<SearchBoxProps> = ({ value, setValue }) => {
   const navigate = useNavigation();
   const goBack = () => {
     navigate.goBack();
   };
+  const valueHandleChange = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>,
+  ) => {
+    setValue(e.nativeEvent.text);
+  };
+
   return (
-    <View className="flex flex-row items-center h-fit w-auto mt-4 mb-3">
-      <View className="flex flex-row flex-1 items-center w-auto p-2 bg-gray100 rounded-xl">
+    <View className="flex flex-row">
+      {/** 검색창 */}
+      <View className="flex flex-row flex-1 h-full p-2 bg-gray100 rounded-xl">
         <View className="px-1">
           <MagnifyingGlassMainSvg></MagnifyingGlassMainSvg>
         </View>
-        <TextInput
-          className="text-center"
-          placeholder="도착지를 검색해주세요"
-        ></TextInput>
+        <View className="flex-col mb-1 justify-center">
+          <TextInput
+            value={value}
+            onChange={valueHandleChange}
+            className="text-base"
+            placeholder="도착지를 검색해주세요"
+          />
+        </View>
       </View>
+
+      {/** 취소 버튼 */}
       <Pressable onPress={goBack}>
         <Text className="text-base p-2.5">취소</Text>
       </Pressable>
--- src/screens/search/Search.tsx
+++ src/screens/search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pressable, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import MagnifyingGlassMainSvg from '@assets/images/Search/MagnifyingGlassMain.svg';
@@ -7,23 +7,35 @@ import SearchBoxComponent from '@components/SearchBox';
 import FilterButtonComponent from '@components/RoomDigest/FilterButton';
 import ChevronDownSvg from '@assets/images/RoomDigest/ChevronDown.svg';
 import LatestSearchComponent from '@components/LatestSearch';
+import InputBoxComponent from '@components/InputBox';
 
 const SearchScreen = () => {
-  const value = '';
+  /** 검색어 저장 변수 */
+  const [keyword, setKeyword] = useState<string>('');
+
   return (
     <SafeAreaView className="flex-1 mx-4">
-      <SearchBoxComponent value={value} />
+      {/** 검색창 */}
+      <View className="mt-4 mb-3">
+        <SearchBoxComponent value={keyword} setValue={setKeyword} />
+      </View>
+
+      {/** 최근검색 탭 */}
       <View className="flex flex-row py-2 px-1">
         <Pressable>
           <Text className="text-lg pr-4 font-semibold">최근 검색</Text>
         </Pressable>
         <Pressable>
-          <Text className="text-lg pr-4 font-medium">거점 리스트</Text>
+          <Text className="text-lg pr-4 font-medium text-gray500">
+            거점 리스트
+          </Text>
         </Pressable>
         <Pressable>
-          <Text className="text-lg font-medium">즐겨찾기</Text>
+          <Text className="text-lg font-medium text-gray500">즐겨찾기</Text>
         </Pressable>
       </View>
+
+      {/** 시군구 태그, 최신순 */}
       <View className="flex flex-row justify-between mt-[6.5px] mb-3">
         <View className="flex flex-row">
           <FilterButtonComponent label="서울특별시" />
@@ -35,7 +47,9 @@ const SearchScreen = () => {
           <ChevronDownSvg />
         </View>
       </View>
-      <View className="flex flex-col">
+
+      {/** 최근 검색어 */}
+      <View className="flex-1">
         <LatestSearchComponent keyword="가양역 1번 출구" distance={500} />
         <LatestSearchComponent keyword="간재울역 4번 출구" distance={500} />
       </View>
