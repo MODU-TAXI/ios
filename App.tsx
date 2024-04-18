1b2f764542cc642079e0597b4d00432382cf7bda
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-04-18 20:30:11 +0900

    MI-81 add react-native-toast-message

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { RecoilRoot } from 'recoil'; // recoil
+import { RecoilRoot } from 'recoil'; // recoil 라이브러리
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { ErrorHandler } from '@components/Fallback/CustomErrorFallback'; // 전역 에러 잡기 error-boundary
+import { ErrorHandler } from '@components/Fallback/CustomErrorFallback'; // 전역 에러 잡기 error-boundary 라이브러리
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // react-query v5 라이브러리
+import Toast from 'react-native-toast-message'; // 400 Error 및 성공 toast message 띄워주기 라이버르리
 
 import AppInner from './AppInner';
 
@@ -12,19 +13,22 @@ const queryClient = new QueryClient(); // react-query client
 
 function App(): React.JSX.Element {
   return (
-    <RecoilRoot>
-      <GestureHandlerRootView>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <QueryClientProvider client={queryClient}>
-              <ErrorHandler>
-                <AppInner />
-              </ErrorHandler>
-            </QueryClientProvider>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </GestureHandlerRootView>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <GestureHandlerRootView>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <QueryClientProvider client={queryClient}>
+                <ErrorHandler>
+                  <AppInner />
+                </ErrorHandler>
+              </QueryClientProvider>
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </GestureHandlerRootView>
+      </RecoilRoot>
+      <Toast />
+    </>
   );
 }
 export default App;
--- src/components/Fallback/CustomErrorFallback.tsx
+++ src/components/Fallback/CustomErrorFallback.tsx
@@ -1,13 +1,36 @@
 import React, { useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { View, StyleSheet, Button, Text } from 'react-native';
-import { useQueryErrorResetBoundary } from '@tanstack/react-query'; // react-query v5 라이브러리
-const myErrorHandler = (error: Error) => {
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import Toast from 'react-native-toast-message';
+
+const myErrorHandler = (error: any) => {
   console.log('error comes to error handler');
   // Do something with the error
 };
 
-function ErrorFallback({ error, resetErrorBoundary }) {
+function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: {
+  error: any;
+  resetErrorBoundary: () => void;
+}) {
+  useEffect(() => {
+    // 400 Error시에는 toast message 띄우기
+    if (error.response.status == 400 && error?.response?.data?.message) {
+      resetErrorBoundary();
+      Toast.show({
+        type: 'error',
+        text1: '에러발생!',
+        text2: error?.response?.data?.message,
+        position: 'bottom',
+      });
+      return;
+    }
+  }, []);
+
+  // 500 에러시에는 해당화면 보여주기
   return (
     <View style={[styles.container]}>
       <View>
