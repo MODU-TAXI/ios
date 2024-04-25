1e6a451c503e261d0a6f5faebc108ccfdad53fec
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-04-25 14:33:01 +0900

    MI-86 add TouchableWithoutFeedback on onboarding

--- src/screens/signUp/SchoolAuthentication.tsx
+++ src/screens/signUp/SchoolAuthentication.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../type/ParamLists';
@@ -37,64 +37,68 @@ const SchoolAuthenticationScreen = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-white" edges={['top', 'left', 'right']}>
-      {/* 진행사항 progressBar */}
-      <View className="h-1 mt-[11px]">
-        <ProgressBarComponent previousDealt={0} dealt={40} />
-      </View>
-
-      <View className="flex-1 mx-6">
-        {/* 입력란 설명 */}
-        <View className="flex mt-14">
-          <Text className="text-xl font-bold">학교 인증을 하면</Text>
-          <Text className="text-xl font-bold">
-            매칭률이 <Text className="text-basic">72% </Text>이상 올라가요!
-          </Text>
-        </View>
-
-        {/* Input 컴포넌트 */}
-        <View className="mt-6">
-          <InputBoxComponent
-            title="학교 이메일"
-            value={email}
-            setValue={setEmail}
-            placeholder="moduteam@inha.edu"
-          />
-        </View>
-
-        {/* 경고 메세지 */}
-        {errorMessage && (
-          <View className="mt-2 px-2">
-            <Text className="text-error font-medium">{errorMessage}</Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View className="flex-1">
+          {/* 진행사항 progressBar */}
+          <View className="h-1 mt-[11px]">
+            <ProgressBarComponent previousDealt={0} dealt={40} />
           </View>
-        )}
 
-        {/* 버튼을 아래로 내리기 위한 View */}
-        <View className="flex-1"></View>
+          <View className="flex-1 mx-6">
+            {/* 입력란 설명 */}
+            <View className="flex mt-14">
+              <Text className="text-xl font-bold">학교 인증을 하면</Text>
+              <Text className="text-xl font-bold">
+                매칭률이 <Text className="text-basic">72% </Text>이상 올라가요!
+              </Text>
+            </View>
 
-        {/* 다음에 하기 버튼 */}
-        <View className="mx-3 mb-3">
-          <ButtonComponent
-            color={'bg-white'}
-            borderColor={'border-disabled'}
-            text={'다음에 할래요'}
-            textColor={'disabled'}
-            onPress={toEnd}
-            disabled={false}
-          />
+            {/* Input 컴포넌트 */}
+            <View className="mt-6">
+              <InputBoxComponent
+                title="학교 이메일"
+                value={email}
+                setValue={setEmail}
+                placeholder="moduteam@inha.edu"
+              />
+            </View>
+
+            {/* 경고 메세지 */}
+            {errorMessage && (
+              <View className="mt-2 px-2">
+                <Text className="text-error font-medium">{errorMessage}</Text>
+              </View>
+            )}
+
+            {/* 버튼을 아래로 내리기 위한 View */}
+            <View className="flex-1"></View>
+
+            {/* 다음에 하기 버튼 */}
+            <View className="mx-3 mb-3">
+              <ButtonComponent
+                color={'bg-white'}
+                borderColor={'border-disabled'}
+                text={'다음에 할래요'}
+                textColor={'disabled'}
+                onPress={toEnd}
+                disabled={false}
+              />
+            </View>
+
+            {/* 확인 버튼 */}
+            <View className="mx-3 mb-11">
+              <ButtonComponent
+                color={'bg-black'}
+                borderColor={'border-black'}
+                textColor={'white'}
+                text={'확인'}
+                disabled={!email}
+                onPress={sendMail}
+              />
+            </View>
+          </View>
         </View>
-
-        {/* 확인 버튼 */}
-        <View className="mx-3 mb-11">
-          <ButtonComponent
-            color={'bg-black'}
-            borderColor={'border-black'}
-            textColor={'white'}
-            text={'확인'}
-            disabled={!email}
-            onPress={sendMail}
-          />
-        </View>
-      </View>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
