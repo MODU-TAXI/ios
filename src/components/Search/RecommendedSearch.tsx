54cedfdaed64e30bf77490bc6255361808328384
Seungbo Shim <fodo9898@gmail.com>
2024-05-11 01:39:57 +0900

    MI-118 검색창 데이터바인딩 준비

---
+++ src/components/Search/RecommendedSearch.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+
+
+interface RecommendedSearchProps {
+  keyword: string;
+  fullKeyword: string;
+  address: string;
+  distance: number;
+}
+
+/** 최근 검색어 컴포넌트 */
+const RecommendedSearchComponent: React.FC<RecommendedSearchProps> = ({
+  keyword,
+  fullKeyword,
+  address,
+  distance,
+}) => {
+  const restKeyword = fullKeyword.replace(keyword, '');
+
+
+  return (
+    <View className="flex flex-row items-center border-b border-gray100 py-3">
+        <View className='ml-2 flex flex-col'>
+
+            <View className='mb-1 flex flex-row items-center'>
+                <Text className="text-[16px] font-medium text-main">{keyword}</Text>
+                <Text className='text-[16px] font-medium text-gray800'>{restKeyword}</Text>
+            </View>
+
+            <View className='flex w-full flex-row justify-between'>
+                <Text className="text-sm text-gray600">{address}</Text>
+                <Text className="text-sm text-gray600">{distance}m</Text>
+            </View>
+        </View>
+    </View>
+  );
+};
+
+export default RecommendedSearchComponent;
--- src/screens/search/Search.tsx
+++ src/screens/search/Search.tsx
@@ -3,13 +3,8 @@ import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text, View, Pressable, TextInput } from 'react-native';
 
-import InputBoxComponent from '@components/InputBox';
-import SearchBoxComponent from '@components/SearchBox';
-import LatestSearchComponent from '@components/LatestSearch';
-import FilterButtonComponent from '@components/RoomDigest/FilterButton';
-
-import ChevronDownSvg from '@assets/images/RoomDigest/ChevronDown.svg';
-import MagnifyingGlassMainSvg from '@assets/images/Search/MagnifyingGlassMain.svg';
+import SearchBoxComponent from '@components/Search/SearchBox';
+import RecommendedSearchComponent from '@components/Search/RecommendedSearch';
 
 const SearchScreen = () => {
   /** 검색어 저장 변수 */
@@ -18,41 +13,26 @@ const SearchScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="mx-4 flex-1">
+        
         {/** 검색창 */}
         <View className="mb-3 mt-4">
           <SearchBoxComponent value={keyword} setValue={setKeyword} />
         </View>
 
-        {/** 최근검색 탭 */}
-        <View className="flex flex-row px-1 py-2">
-          <Pressable>
-            <Text className="pr-4 text-lg font-semibold">최근 검색</Text>
-          </Pressable>
-          <Pressable>
-            <Text className="pr-4 text-lg font-medium text-gray500">거점 리스트</Text>
-          </Pressable>
-          <Pressable>
-            <Text className="text-lg font-medium text-gray500">즐겨찾기</Text>
-          </Pressable>
-        </View>
-
-        {/** 시군구 태그, 최신순 */}
-        <View className="mb-3 mt-[6.5px] flex flex-row justify-between">
-          <View className="flex flex-row">
-            <FilterButtonComponent label="서울특별시" />
-            <FilterButtonComponent label="강서구" />
-            <FilterButtonComponent label="주안역" />
-          </View>
-          <View className="flex flex-row items-center">
-            <Text className="pr-1 text-gray700">최신순</Text>
-            <ChevronDownSvg />
-          </View>
-        </View>
-
-        {/** 최근 검색어 */}
+        {/** 추천 검색어 */}
         <View className="flex-1">
-          <LatestSearchComponent keyword="가양역 1번 출구" distance={500} />
-          <LatestSearchComponent keyword="간재울역 4번 출구" distance={500} />
+          <RecommendedSearchComponent 
+            keyword='주안역' 
+            fullKeyword='주안역 센트리빌' 
+            address='인천 미추홀구 주안로 41번길' 
+            distance={500}
+          />
+          <RecommendedSearchComponent 
+            keyword='주안역' 
+            fullKeyword='주안역 3동 성모마리아 성당' 
+            address='인천 미추홀구 주안로 41번길' 
+            distance={500}
+          />
         </View>
       </View>
     </SafeAreaView>
