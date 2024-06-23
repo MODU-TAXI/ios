47726aea2159473698e76ee611b2142dd9bd05dc
Seungbo Shim <fodo9898@gmail.com>
2024-06-23 18:24:15 +0900

    MI-316 fix patchRoom time

--- src/components/Home/Party.tsx
+++ src/components/Home/Party.tsx
@@ -18,7 +18,7 @@ const PartyComponent: React.FC<PartyComponentProps> = ({ roomDetail }) => {
       {/* 출발 시각 */}
       <View className="w-[100px] flex-row items-center justify-center rounded-lg bg-[#EBFBF7] px-3 py-1">
         <Check className="mr-1" width={12} />
-        <Text className="text-[12px] font-medium text-main">출발 {roomDetail.arrivalTime}</Text>
+        <Text className="text-[12px] font-medium text-main">출발 {roomDetail.departureTime}</Text>
       </View>
 
       {/* 출발지, 도착지 */}
