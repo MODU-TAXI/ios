77ee280ff559f9773bdef54b823f1562eac5162c
seungboshim <fodo9898@gmail.com>
2024-09-29 18:06:20 +0900

    MI-429 specify recoil types

--- src/screens/search/MainMapSearch.tsx
+++ src/screens/search/MainMapSearch.tsx
@@ -10,6 +10,7 @@ import SearchBoxComponent from '@components/Search/SearchBox';
 import RecommendedSearchComponent from '@components/Search/RecommendedSearch';
 import EmptySearchRenderComponent from '@components/Search/EmptySearchRender';
 
+import { SearchParamRecoil } from '@recoil/types/search';
 import { searchParamRecoilState } from '@recoil/states/search';
 
 import { useNaverSearch } from '@hooks/api/search';
@@ -37,7 +38,7 @@ const SearchScreen = ({ navigation }: SearchScreenProps) => {
   });
 
   // 검색어 선택시 넘겨줄 값
-  const [, setSearchParamRecoil] = useRecoilState(searchParamRecoilState);
+  const [, setSearchParamRecoil] = useRecoilState<SearchParamRecoil>(searchParamRecoilState);
 
   useEffect(() => {
     const fetchCurrentLocation = async () => {
